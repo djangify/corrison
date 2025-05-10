d63685a2436d4f2f96c221e780f75a60ec998fa6
djangify <djangify@gmail.com>
2025-05-10 19:43:45 +0100

    Add session key

--- corrison/settings/local.py
+++ corrison/settings/local.py
@@ -18,14 +18,58 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 # Allow requests from your frontend domain
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://ecommerce.todiane.com",
-    # Add any other domains you need
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    # Add any other domains yo
+    # u need
 ]
+
+# These are the critical ones for cookies
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = False  # Don't use this in production, but might help locally
+
+# Session cookie settings
+SESSION_COOKIE_NAME = 'sessionid'
+SESSION_COOKIE_SAMESITE = 'Lax'  # Try 'None' if 'Lax' doesn't work
+SESSION_COOKIE_SECURE = False    # False for HTTP in development
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_PATH = '/'
+SESSION_SAVE_EVERY_REQUEST = True  # Force session save on every request
+
+# Add CORS headers explicitly
+CORS_EXPOSE_HEADERS = [
+    'Content-Type',
+    'X-CSRFToken',
+    'Set-Cookie',
+]
+
+
+# Debug settings
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+# Add this to help with debugging
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
 
 pymysql.install_as_MySQLdb()
 DATABASES = {
