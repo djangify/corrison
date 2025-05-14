e18b06719055b576c95fde62f39fb6d5f1591c48
djangify <djangify@gmail.com>
2025-05-14 10:46:55 +0100

    rebuild docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 exec gunicorn 'corrison.wsgi' --bind=0.0.0.0:3000 --timeout 120 --workers 3
